978b3852feaf6bd48a91d44de61d3fe47d7de972
kareemnour <kareemnour221@gmail.com>
2024-04-23 06:14:42 +0200

    New Feature

---
+++ Test Your Self App/main.js
@@ -0,0 +1,12 @@
+// window.onload = function () {
+//     const music = new Audio ("./Sounds/موسيقى توتر عميقة تقشعر لها الأبدان.mp3")
+//     music.play()
+// }
+const img = document.querySelector("#img");
+console.log(img);
+let imgArray = [1,2,3];
+setInterval(() => {
+    let index = Math.floor(Math.random() * imgArray.length);
+    console.log(index);
+    img.src = `./Images/${index + 1}.jpg`;
+}, 3000);
